134a8fb46be932bde018f48661a087d616baef33
Henry Alejandro <henryalejandro1229@gmail.com>
2023-04-11 00:21:24 -0600

    Fix - singup

--- api/users/singup.php
+++ api/users/singup.php
@@ -4,10 +4,14 @@ require "../config/database.php";
 class query extends Conexion {
     public function config() {
         $email = $_GET['email'];
+        $name = $_GET['name'];
+        $apellido = $_GET['apellido'];
+        $edad = $_GET['edad'];
+        $password = $_GET['password'];
         try {
             $conexion = parent::conectar();
             $query = new MongoDB\Driver\BulkWrite;
-            $query->insert(['email'=>$email]);
+            $query->insert(['email'=>$email, 'name'=>$name, 'apellido'=>$apellido, 'edad'=>$edad, 'password'=>$password]);
             $conexion->executeBulkWrite($this->database_name.$this->col_users, $query);
         } catch (\Throwable $th) {
             return $th->getMessage();

c45ccded962004cde8db276cc4c59021f9d42894
Henry Alejandro <henryalejandro1229@gmail.com>
2023-08-20 19:19:52 -0600

    Fix - Singup

--- api/users/singup.php
+++ api/users/singup.php
@@ -8,11 +8,18 @@ class query extends Conexion {
         $apellido = $_GET['apellido'];
         $edad = $_GET['edad'];
         $password = $_GET['password'];
+        $sessionGoogle = $_GET['sessionGoogle']; 
+        $bool_val = ($sessionGoogle === "true" || $sessionGoogle === true);
         try {
             $conexion = parent::conectar();
             $query = new MongoDB\Driver\BulkWrite;
-            $query->insert(['email'=>$email, 'name'=>$name, 'apellido'=>$apellido, 'edad'=>$edad, 'password'=>$password]);
+            $query->insert(['email'=>$email, 'name'=>$name, 'apellido'=>$apellido, 'edad'=>$edad, 'password'=>$password, 'sessionGoogle'=>$bool_val]);
             $conexion->executeBulkWrite($this->database_name.$this->col_users, $query);
+
+            $conn = parent::conectar();
+            $query = new MongoDB\Driver\Query(['email' => $email]);
+            $cursor = $conn->executeQuery($this->database_name.$this->col_users, $query);
+            return json_decode(json_encode($cursor->toArray()),JSON_UNESCAPED_UNICODE);
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
